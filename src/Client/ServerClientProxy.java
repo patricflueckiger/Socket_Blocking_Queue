35ecf9f4ebd8d8c59f8f17fa1b3fa07dd8335bd4
celinemonigadon <celine.monigadon@gmx.ch>
2018-11-20 13:33:56 +0100

    added class ServerProxy and ServerClientProxy

---
+++ src/Client/ServerClientProxy.java
@@ -0,0 +1,4 @@
+
+public class ServerClientProxy extends ServerProxy{
+
+}
---
+++ src/Client/ServerProxy.java
@@ -0,0 +1,14 @@
+package network.client.gruppe1;
+
+import network.Message;
+import network.client.ClientApplicationInterface;
+
+public class ServerProxy {
+    public ServerProxy(ClientApplicationInterface clientApplicationInterface){
+
+    }
+
+    public void send(Message message) {
+
+    }
+}
