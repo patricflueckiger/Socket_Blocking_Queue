be47a52c051d7bad4a1d46a7c74f0ce024db2c29
celinemonigadon <celine.monigadon@gmx.ch>
2018-11-20 13:53:49 +0100

    structure

--- src/Client/ServerClientProxy.java
+++ src/Client/ServerClientProxy.java
@@ -1,4 +1,33 @@
+import network.Message;
+import network.client.ClientApplicationInterface;
 
 public class ServerClientProxy extends ServerProxy{
+	
+	public ClientServerProxy(ClientApplicationInterface clientApplication) {
+		super(clientApplication);
+		// TODO Auto-generated constructor stub
+	}
 
+	@Override
+	public void send(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+	private void deliverResponseMessageToClient(Message responseMessage) {
+		Thread responseThread = new Thread() {
+			@Override
+			public void run() {
+				clientApplication.handleMessage(responseMessage);
+			}
+		};
+		responseThread.start();
+	}
+
+//	public void openConnection() {
+//		//socket eröffnen message an server 
+//		
+//	}
+//	public void closeConnection() {
+//		
+//	} 
 }
