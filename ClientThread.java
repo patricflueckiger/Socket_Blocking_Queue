9b52a16bcedba8bbe24f6d95248ff17280ef027f
celinemonigadon <celine.monigadon@gmx.ch>
2018-12-11 14:32:32 +0100

    clientthread

---
+++ ClientThread.java
@@ -0,0 +1,77 @@
+package src.Server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class ClientThread extends Thread
+{
+
+	private ServerSocket ss;
+	private String threadName;
+	private boolean connected; 
+	private Observable observable;
+	
+	public ClientThread(ServerSocket ss, String name, Observer observer) 
+	{
+		this.ss = ss;
+		this.threadName = name;
+		this.observable = new Observable();
+		this.observable.addObserver(observer);
+		this.connected = false;
+	}
+	
+	
+	
+	
+	public boolean isConnected() 
+	{
+		return this.connected;
+	}
+	
+	public void run()
+	{
+		this.threadName = Thread.currentThread().getName();
+		System.out.println("Server wartet auf Verbindung ("+threadName+")");
+		
+		sendMessage("");
+		
+	}
+	
+	public void sendMessage(String message)
+	{
+		try {
+			Socket cs = ss.accept(); 
+		
+			ObjectInputStream is = new ObjectInputStream(cs.getInputStream());
+			ObjectOutputStream os = new ObjectOutputStream(cs.getOutputStream());
+		
+			this.connected = true;
+			System.out.println("Verbindung zum ClientSocket wurde aufgebaut.");
+		
+			os.writeObject(message);
+			
+			observable.notifyObservers(is.readObject());
+			
+			is.close();
+			os.close();
+			cs.close();
+			
+			this.connected = false;
+			System.out.println("Verbindung wurde abgebaut ("+threadName+")");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+}
--- src/Server/ServerServer.java
+++ src/Server/ServerServer.java
@@ -12,7 +12,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.io.*;
 import java.util.function.Consumer;
 
-public final class ServerServer extends Server {
+public final class ServerServer extends Server implements Observer{
 
     private static volatile ServerServer serverInstance;
 	protected int port = 4444;
@@ -21,6 +21,8 @@ public final class ServerServer extends Server {
 	protected HashMap<String, Socket> sockets;
 	ExecutorService executor = Executors.newFixedThreadPool(5);
 	BlockingQueue<TaskItem> queue = new LinkedBlockingDeque<>();
+	private int maxClient = 4;
+	protected List<ClientThread> clients = new List<ClientThread>();
 
 	//Singleton Design Pattern
 	public static ServerServer getInstance(ServerApplicationInterface serverApplicationInterface){
@@ -58,9 +60,14 @@ public final class ServerServer extends Server {
 	}
 	
 	public void openConnection(String connectionString) {
-
+		
 		try {
-			sockets.put(connectionString, serverSocket.accept()); 
+			ServerSocket serverSocket = new ServerSocket(port);
+			
+			for(int i=0; i < maxClient; i++) {
+				clients.Add(new ClientThread());
+			}
+			sockets.put(connectionString, serverSocket.accept(serverSocket, i++.toString(), this)); 
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -73,6 +80,11 @@ public final class ServerServer extends Server {
         //Implement sending Task
 		return null;
 	}
+	
+	@Override
+    public void update(Observable o, Message message) {
+		serverApplication.handleMessage(message);
+    }
 
 
 
